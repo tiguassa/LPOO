164877f09161c0b5d81ff9e1d0e261e117cd33b0
strowfilds <leonardo.wrobel@gmail.com>
2019-03-22 17:02:14 -0300

    classe auxiliar importada com sucesso

--- 04/Exercicio_02.java
+++ 04/Exercicio_02.java
@@ -2,6 +2,8 @@ import classesaux.Pessoa;
 
 public class Exercicio_02 {
 	public static void main(String args[]){
-		Pessoa pessoa01 = new Pessoa();
+		Pessoa pessoa01 = new Pessoa("Leonardo", "Rua estação da Luz, 354", 27);
+
+		System.out.println("Olá, meu nome é " + pessoa01.getNome() + "!");
 	}
 }
