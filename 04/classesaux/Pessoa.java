9cdaa8d73be16a538a556892821ed771de072ec6
strowfilds <leonardo.wrobel@gmail.com>
2019-03-22 16:58:58 -0300

    classe pessoas done

--- 04/classesaux/Pessoa.java
+++ 04/classesaux/Pessoa.java
@@ -2,4 +2,32 @@ package classesaux;
 
 public class Pessoa{
 	
+	public Pessoa(String nome, String endereco, int idade) {
+		setNome(nome);
+		setEndereco(endereco);
+		setIdade(idade);
+	}
+
+	private String nome;
+	private String endereco;
+	private int idade;
+
+	public setNome(String nome){
+		this.nome = nome;
+	}
+	public setEndereco(String endereco){
+		this.endereco = endereco;
+	}
+	public setIdade(int idade){
+		this.idade = idade;
+	}
+	public getNome(){
+		return this.nome;
+	}
+	public getEndereco(){
+		return this.endereco;
+	}
+	public getIdade(){
+		return this.idade;
+	}
 }
