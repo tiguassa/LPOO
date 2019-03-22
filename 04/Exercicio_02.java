e780a0c65751d22377c985fea16b030b17dfa1ff
strowfilds <leonardo.wrobel@gmail.com>
2019-03-22 16:41:40 -0300

    iniciando lista 04

---
+++ 04/Exercicio_02.java
@@ -0,0 +1,7 @@
+import classesaux.Pessoa;
+
+public class Exercicio_02 {
+	public static void main(String args[]){
+		Pessoa pessoa01 = new Pessoa();
+	}
+}
---
+++ 04/classesaux/Pessoa.java
@@ -0,0 +1,5 @@
+package classesaux;
+
+public class Pessoa{
+	
+}
