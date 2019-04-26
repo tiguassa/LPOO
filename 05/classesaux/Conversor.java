d7ffbaf1d2a0348558f7512da4acbb91dc51b944
strowfilds <leonardo.wrobel@gmail.com>
2019-04-26 12:40:48 -0300

    conversor ok

--- 05/classesaux/Conversor.java
+++ 05/classesaux/Conversor.java
@@ -1,7 +1,27 @@
 package classesaux;
 
-public static class Conversor {
+public class Conversor {
 	public static double c2f(double c){
 		return (c * (9/5) + 32);
+	}
+
+	public static double c2k(double c){
+		return (c + 273.15);
+	}
+
+	public static double f2c(double f){
+		return ((f - 32) * (5/9));
+	}
+
+	public static double f2k(double f){
+		return ((f + 459.67) * (5/9));
+	}
+
+	public static double k2c(double k){
+		return (k - 273.15);
+	}
+
+	public static double k2f(double k){
+		return ((k * (9/5)) - 459.67);
 	}
 }
