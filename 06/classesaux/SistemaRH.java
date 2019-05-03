90bb25c2bcec8c841632da2861cf46c5eb39f0e2
strowfilds <leonardo.wrobel@gmail.com>
2019-05-03 14:28:05 -0300

    inicio exerc3 e sistRH

--- 06/classesaux/SistemaRH.java
+++ 06/classesaux/SistemaRH.java
@@ -2,5 +2,11 @@ package classesaux;
 import java.util.Scanner;
 
 public class SistemaRH {
-	public static void imprimeRelatorio(Funcionario[] listaFuncionarios);
+	public static void imprimeMenu(){
+		System.out.println("= = = = = = = Sistema para gestão de RH = = = = = = = ");
+	}
+
+	public static void imprimeRelatorio(Funcionario[] listaFuncionarios){
+
+	}
 }
