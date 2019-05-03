945553621b3ef26c7fe864449bed117fa1ccd0f5
strowfilds <leonardo.wrobel@gmail.com>
2019-05-03 14:27:50 -0300

    inicio exerc3 e sistRH

---
+++ 06/Exercicio_03.java
@@ -0,0 +1,10 @@
+import classesaux.*;
+import java.util.Scanner;
+
+public class Exercicio_03 {
+	public static void main(String args[]){
+		Scanner scan = new Scanner(System.in);
+		
+
+	}
+}
---
+++ 06/classesaux/SistemaRH.java
@@ -0,0 +1,6 @@
+package classesaux;
+import java.util.Scanner;
+
+public class SistemaRH {
+	public static void imprimeRelatorio(Funcionario[] listaFuncionarios);
+}
