a4539c70b56a8487791b759ca8c3e667209ec78d
strow <leonardo.wrobel@gmail.com>
2019-05-09 20:22:46 -0300

    asdafasdn

--- 06/Exercicio_03.java
+++ 06/Exercicio_03.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Exercicio_03 {
 	public static void main(String args[]){
-		Scanner scan = new Scanner(System.in);
+		
 		
 
 	}
--- 06/classesaux/SistemaRH.java
+++ 06/classesaux/SistemaRH.java
@@ -1,7 +1,11 @@
 package classesaux;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class SistemaRH {
+	Scanner scan = new Scanner(System.in);
+	funcionarios = new ArrayList<Funcionario>();
+
 	public static void imprimeMenu(){
 		System.out.println("= = = = = = = Sistema para gestão de RH = = = = = = = ");
 	}
