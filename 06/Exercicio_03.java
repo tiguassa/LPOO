a7ef7dfee5ea9b851318e9a6fd33613a72388e0a
strowfilds <leonardo.wrobel@gmail.com>
2019-05-14 16:04:25 -0300

    deu ruim no ultimo commit

--- 06/Exercicio_03.java
+++ 06/Exercicio_03.java
@@ -1,10 +1,61 @@
 import classesaux.*;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Exercicio_03 {
 	public static void main(String args[]){
-		
-		
+		Scanner scan = new Scanner(System.in);
+		ArrayList<Funcionario> funcionarios;
 
+		char op;
+		
+		while(true){
+			SistemaRH.imprimeMenu();
+			op = scan.next().charAt(0);
+			if(op == '9')
+				break;
+			switch(op){
+				case '1':
+					while(true){
+						SistemaRH.imprimeMenuFuncionario();
+						op = scan.next().charAt(0);
+						if(op == '9')
+							break;
+						switch(op){
+							case '1':
+								
+								break;
+							case '2':
+
+								break;
+							case '3':
+
+								break;
+							case '4':
+
+								break;
+							default:
+								System.out.println("        # Opcao invalida!");
+								break;
+						}
+					}
+					break;
+				case '2':
+
+					break;
+				case '3':
+
+					break;
+				case '4':
+
+					break;
+				case '5':
+
+					break;
+				default:
+					System.out.println("        # Opcao invalida!");
+					break;
+			}
+		}
 	}
 }
--- 06/classesaux/SistemaRH.java
+++ 06/classesaux/SistemaRH.java
@@ -1,16 +1,127 @@
 package classesaux;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class SistemaRH {
-	Scanner scan = new Scanner(System.in);
-	funcionarios = new ArrayList<Funcionario>();
-
 	public static void imprimeMenu(){
-		System.out.println("= = = = = = = Sistema para gestão de RH = = = = = = = ");
+		System.out.println("+ = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = +");
+		System.out.println("|                                RH - Menu                            |");
+		System.out.println("+ = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = +");
+		System.out.println("| Entre com a opção desejada:                                         |");
+		System.out.println("+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +");
+		System.out.println("|                                                                     |");
+		System.out.println("|      1 - Editar funcionarios                                        |");			
+		System.out.println("|      2 - Editar departamentos                                       |");			
+		System.out.println("|      3 - Listar Funcionarios                                        |");			
+		System.out.println("|      4 - Listar departamentos                                       |");			
+		System.out.println("|      5 - Imprimir relatorio                                         |");			
+		System.out.println("|      9 - Fim                                                        |");			
+		System.out.println("|                                                                     |");
+		System.out.println("+ = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = +");
+		System.out.print("        Opção: ");			
 	}
 
+	public static void imprimeMenuFuncionario(){
+		System.out.println("+ = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = +");
+		System.out.println("|                                RH - Menu [Funcionarios]             |");
+		System.out.println("+ = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = +");
+		System.out.println("| Entre com a opção desejada:                                         |");
+		System.out.println("+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +");
+		System.out.println("|                                                                     |");
+		System.out.println("|      1 - Adicionar funcionario                                      |");			
+		System.out.println("|      2 - Remover funcionario                                        |");			
+		System.out.println("|      3 - Editar funcionario                                         |");			
+		System.out.println("|      4 - Listar funcionarios                                        |");			
+		System.out.println("|      9 - Voltar                                                     |");			
+		System.out.println("|                                                                     |");
+		System.out.println("+ = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = +");
+		System.out.print("        Opção: ");		
+	}
+
+	public static void imprimeMenuDepartamento(){
+		System.out.println("+ = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = +");
+		System.out.println("|                                RH - Menu [Funcionarios]             |");
+		System.out.println("+ = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = +");
+		System.out.println("| Entre com a opção desejada:                                         |");
+		System.out.println("+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +");
+		System.out.println("|                                                                     |");
+		System.out.println("|      1 - Adicionar departamento                                     |");			
+		System.out.println("|      2 - Remover departamento                                       |");			
+		System.out.println("|      3 - Editar departamento                                        |");			
+		System.out.println("|      4 - Listar departamentos                                       |");			
+		System.out.println("|      9 - Voltar                                                     |");			
+		System.out.println("|                                                                     |");
+		System.out.println("+ = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = +");
+		System.out.print("        Opção: ");		
+	}
+
+	public static String[] userInputNomeCpf(){
+
+	}
+	/*
+	public static Funcionario cadastrarFuncionario(){
+
+	}
+	*/
 	public static void imprimeRelatorio(Funcionario[] listaFuncionarios){
 
 	}
+
+	// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+	public static boolean isCPF(String CPF) {
+        // considera-se erro CPF's formados por uma sequencia de numeros iguais
+	    if (CPF.equals("00000000000") ||
+	        CPF.equals("11111111111") ||
+	        CPF.equals("22222222222") || CPF.equals("33333333333") ||
+	        CPF.equals("44444444444") || CPF.equals("55555555555") ||
+	        CPF.equals("66666666666") || CPF.equals("77777777777") ||
+	        CPF.equals("88888888888") || CPF.equals("99999999999") ||
+	        (CPF.length() != 11))
+	        return(false);
+	      
+	    char dig10, dig11;
+	    int sm, i, r, num, peso;
+	      
+	    // "try" - protege o codigo para eventuais erros de conversao de tipo (int)
+	    try {
+	    // Calculo do 1o. Digito Verificador
+	        sm = 0;
+	        peso = 10;
+	        for (i=0; i<9; i++) {              
+	    // converte o i-esimo caractere do CPF em um numero:
+	    // por exemplo, transforma o caractere '0' no inteiro 0         
+	    // (48 eh a posicao de '0' na tabela ASCII)         
+	        num = (int)(CPF.charAt(i) - 48); 
+	        sm = sm + (num * peso);
+	        peso = peso - 1;
+	        }
+	      
+	        r = 11 - (sm % 11);
+	        if ((r == 10) || (r == 11))
+	            dig10 = '0';
+	        else dig10 = (char)(r + 48); // converte no respectivo caractere numerico
+	      
+	    // Calculo do 2o. Digito Verificador
+	        sm = 0;
+	        peso = 11;
+	        for(i=0; i<10; i++) {
+	        num = (int)(CPF.charAt(i) - 48);
+	        sm = sm + (num * peso);
+	        peso = peso - 1;
+	        }
+	      
+	        r = 11 - (sm % 11);
+	        if ((r == 10) || (r == 11))
+	             dig11 = '0';
+	        else dig11 = (char)(r + 48);
+	      
+	    // Verifica se os digitos calculados conferem com os digitos informados.
+	        if ((dig10 == CPF.charAt(9)) && (dig11 == CPF.charAt(10)))
+	             return(true);
+	        else return(false);
+	    } catch (InputMismatchException erro) {
+	        return(false);
+	    }
+		// https://www.devmedia.com.br/validando-o-cpf-em-uma-aplicacao-java/22097
+	} 
 }
